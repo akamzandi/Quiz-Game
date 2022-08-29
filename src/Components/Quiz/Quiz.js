207d40ee5c9f8e3dfccc7e6491878c300222f0d8
Akam Zandi <zandi.dev@gmail.com>
2022-08-29 18:38:28 +0430

    shuffled answer options before render

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -2,14 +2,36 @@ import AnswerOption from "../AnswerOption/AnswerOption";
 import "./Quiz.css";
 
 const Quiz = ({ question, correct_answer, all_answers, id }) => {
-  const answerOptionComponents = all_answers.map((item) => (
-    <AnswerOption info={item} />
-  ));
+  const shuffleArray = (array) => {
+    let currentIndex = array.length,
+      randomIndex;
+
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+
+    return array;
+  };
+
+  const renderQuestion = () => {
+    return `${id + 1}) ${question}`;
+  };
+
+  const renderShuffledAnswerOptions = () => {
+    shuffleArray(all_answers);
+    return all_answers.map((item) => <AnswerOption info={item} />);
+  };
 
   return (
     <div className="quiz-container">
-      <h3 className="quiz-question">{`${id + 1}) ${question}`}</h3>
-      <div className="answers-section">{answerOptionComponents}</div>
+      <h3 className="quiz-question">{renderQuestion()}</h3>
+      <div className="answers-section">{renderShuffledAnswerOptions()}</div>
     </div>
   );
 };
