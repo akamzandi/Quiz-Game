ef157ce3181eb6c4250937377051f8a298fbca55
Akam Zandi <zandi.dev@gmail.com>
2022-08-29 14:54:04 +0430

    AnswerOption component created

---
+++ src/Components/AnswerOption/AnswerOption.js
@@ -0,0 +1,7 @@
+import "./AnswerOption.css";
+
+const AnswerOption = () => {
+  return <div className="answer-option">Hi!</div>;
+};
+
+export default AnswerOption;
--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -1,10 +1,16 @@
+import AnswerOption from "../AnswerOption/AnswerOption";
 import "./Quiz.css";
 
 const Quiz = () => {
   return (
     <div className="quiz-container">
       <h3 className="quiz-question">How would one say good bye in spanish?</h3>
-      <div className="answers-section">some shit , hallo, hi , ciao</div>
+      <div className="answers-section">
+        <AnswerOption />
+        <AnswerOption />
+        <AnswerOption />
+        <AnswerOption />
+      </div>
     </div>
   );
 };
