52858b93961a4566d2c9e540a40ba5780a3e8382
Akam Zandi <zandi.dev@gmail.com>
2022-08-31 18:01:18 +0430

    final answer options style completed

--- src/Components/AnswerOption/AnswerOption.js
+++ src/Components/AnswerOption/AnswerOption.js
@@ -9,17 +9,31 @@ const AnswerOption = ({
   isSelected,
   correct_answer,
 }) => {
+  const getAnswerOptionStatus = () => {
+    if (isSelected && text == correct_answer) {
+      return "correct";
+    } else if (isSelected && text !== correct_answer) {
+      return "wrong";
+    } else {
+      return "neuter";
+    }
+  };
+
   const renderAnswerOptionsStyle = () => {
     if (gameRunningState) {
       return isSelected ? "answer-option selected-option" : "answer-option";
     } else {
-      // wrong , true, neuter
-      if (isSelected && text == correct_answer) {
-        return "answer-option correct-answer";
-      } else if (isSelected && text !== correct_answer) {
-        return "answer-option wrong-answer";
-      } else {
-        return "answer-option neuter-answer";
+      const answerOptionStatus = getAnswerOptionStatus();
+      if (answerOptionStatus == "correct") {
+        return "correct-choice";
+      } else if (answerOptionStatus == "wrong") {
+        return "wrong-choice";
+      } else if (answerOptionStatus == "neuter") {
+        if (text == correct_answer) {
+          return "correct-answer";
+        } else {
+          return "neuter-choice";
+        }
       }
     }
   };
